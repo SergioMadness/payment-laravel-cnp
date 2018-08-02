933a511da4f68646eb20dacf301e69db7d45dab1
SergioMadness <madness.sergio@gmail.com>
2018-08-02 15:29:02 +0300

    init commit

---
+++ src/drivers/cnp/CnpProtocol.php
@@ -0,0 +1,167 @@
+<?php namespace professionalweb\payment\drivers\cnp;
+
+use professionalweb\payment\contracts\PayProtocol;
+
+/**
+ * Wrapper for CNP protocol
+ * @package professionalweb\payment\drivers\cnp
+ */
+class CnpProtocol implements PayProtocol
+{
+    /**
+     * Current payment URL
+     *
+     * @var string
+     */
+    private $paymentUrl;
+
+    /**
+     * @var string
+     */
+    private $merchantId;
+
+    /**
+     * @var string
+     */
+    private $terminalId;
+
+    public function __construct($url = '', $merchantId = '', $terminalId = '')
+    {
+        $this
+            ->setPaymentUrl($url)
+            ->setMerchantId($merchantId)
+            ->setTerminalId($terminalId);
+    }
+
+    /**
+     * Get payment URL
+     *
+     * @param mixed $params
+     *
+     * @return string
+     */
+    public function getPaymentUrl($params)
+    {
+        $params = array_merge([
+            'MerchantID' => $this->getMerchantId(),
+            'TerminalId' => $this->getTerminalId(),
+        ], $params);
+
+        $soap = new \SoapClient($this->getPaymentGateUrl());
+        $response = $soap->startTransaction($params);
+
+        return '';
+    }
+
+    /**
+     * Validate params
+     *
+     * @param mixed $params
+     *
+     * @return bool
+     */
+    public function validate($params)
+    {
+        return true;
+    }
+
+    /**
+     * Get payment ID
+     *
+     * @return mixed
+     */
+    public function getPaymentId()
+    {
+        // TODO: Implement getPaymentId() method.
+    }
+
+    /**
+     * Prepare response on notification request
+     *
+     * @param mixed $requestData
+     * @param int   $errorCode
+     *
+     * @return string
+     */
+    public function getNotificationResponse($requestData, $errorCode)
+    {
+        return response('');
+    }
+
+    /**
+     * Prepare response on check request
+     *
+     * @param array $requestData
+     * @param int   $errorCode
+     *
+     * @return string
+     */
+    public function getCheckResponse($requestData, $errorCode)
+    {
+        return $this->getNotificationResponse($requestData, $errorCode);
+    }
+
+    /**
+     * Set payment url
+     *
+     * @param string $url
+     *
+     * @return $this
+     */
+    public function setPaymentUrl($url)
+    {
+        $this->paymentUrl = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get payment gate URL
+     *
+     * @return string
+     */
+    public function getPaymentGateUrl()
+    {
+        return $this->paymentUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMerchantId()
+    {
+        return $this->merchantId;
+    }
+
+    /**
+     * @param string $merchantId
+     *
+     * @return $this
+     */
+    public function setMerchantId($merchantId)
+    {
+        $this->merchantId = $merchantId;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTerminalId()
+    {
+        return $this->terminalId;
+    }
+
+    /**
+     * @param string $terminalId
+     *
+     * @return $this
+     */
+    public function setTerminalId($terminalId)
+    {
+        $this->terminalId = $terminalId;
+
+        return $this;
+    }
+}
---
+++ src/interfaces/CnpService.php
@@ -0,0 +1,10 @@
+<?php namespace professionalweb\payment\interfaces;
+
+/**
+ * Interface for CNP Processing GmbH service
+ * @package professionalweb\payment\interfaces
+ */
+interface CnpService
+{
+    const PAYMENT_CNP = 'cnp';
+}
